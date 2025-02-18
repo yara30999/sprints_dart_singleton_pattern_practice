0ec85bf5a6402333b7ee115ba22639bc95aad70e
yara30999 <yaranaserelniny@gmail.com>
2025-02-18 16:44:57 +0200

    singleton.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# 🔂 Singleton Pattern Practice
+
+![Status](https://img.shields.io/badge/Status-Active-brightgreen)
+![Dart](https://img.shields.io/badge/Dart-100%25-brightgreen)
+
+A simple Dart program to practice singleton pattern.
+
+## 📋 Sample Output
+
+<img src="lib/output.png" >
--- lib/sprints_dart_singleton_practice.dart
+++ lib/sprints_dart_singleton_practice.dart
@@ -1,1 +1,47 @@
+class Database {
+  final int id;
+  final String name;
+  String? description = '';
+  Database._internal(this.id, this.name);
 
+  static Database? _instance;
+
+  factory Database() {
+    if (_instance == null) {
+      _instance = Database._internal(1, 'yara');
+      return _instance!;
+    } else {
+      return _instance!;
+    }
+  }
+
+  void printDatabaseInfo({String? descriptionLabel}) {
+    description = descriptionLabel;
+    print(
+      "$description database with id= $id and name= $name has been created!",
+    );
+  }
+}
+
+class Client {
+  void checkSingleton() {
+    Database db1 = Database();
+    Database db2 = Database();
+
+    db1.printDatabaseInfo(descriptionLabel: 'first object');
+    db2.printDatabaseInfo(
+      descriptionLabel: 'second object is the same as the first',
+    );
+
+    if (db1.description == db2.description) {
+      print("Both instances are the same. Singleton applied successfully!");
+    } else {
+      print("Instances are different. Singleton implementation failed!");
+    }
+  }
+}
+
+void main() {
+  Client client = Client();
+  client.checkSingleton();
+}
